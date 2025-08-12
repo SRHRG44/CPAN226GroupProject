9c1dd55ad3cbbd7573bbca5b863797ee2efae0c0
Sergio Romero <sergior8992@gmail.com>
2025-08-12 13:07:29 -0400

    fiex async but now need to update DB to save chats

--- Chat_App/chatapp/consumers.py
+++ Chat_App/chatapp/consumers.py
@@ -6,16 +6,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -25,21 +22,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username = self.scope['user'].username # Get the username from the scope
 
-        # Send message to room group
+        # Send message and username to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'username': username, # Pass the username here
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        username = event['username'] # Receive the username
 
-        # Send message to WebSocket
+        # Send message and username to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'username': username, # Send the username to the client
         }))
